3dba79c60ae733e240d34a66547f4011e34141b8
vivat <vivatsathorn@outlook.co.th>
2024-06-10 13:41:45 -0600

    Ready for LUNA GCS

--- deps/gui/mymenubar.py
+++ deps/gui/mymenubar.py
@@ -6,7 +6,7 @@ MenuBar = html.Div([
             dbc.NavItem(dbc.NavLink('Home', href='/', active='exact')),
             dbc.NavItem(dbc.NavLink('Settings', href='/settings', active='exact')),
             dbc.NavItem(dbc.NavLink('Info', href='/info', active='exact')),
-            dbc.NavItem(dbc.NavLink('Visit GitHub', href='https://github.com/vtneil/vnet-ugcs',
+            dbc.NavItem(dbc.NavLink('Visit GitHub', href='https://github.com/vtneil/vt-ugcs',
                                     target='_blank', external_link=True)),
         ], brand=APP_TITLE, brand_href='/',
             color='dark', dark=True, fixed='top')
